da123ef435648bc52687da8f4a3c5df2ce981e0e
Akashi-242 <teamsasaki1@yahoo.com>
2024-09-14 14:36:30 +0100

    Add files via upload

---
+++ wasiqr.js
@@ -0,0 +1,107 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Wasi_Tech,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function WASI_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+	
+				   let WASI_MD_TEXT = `┏━━━━━━━━━━━━━━⍟
+┃𝘿𝙚𝙖𝙩𝙝𝙨𝙜𝙪𝙣 SUCCESSFULLY 
+┃CONNECTED ✅
+┗━━━━━━━━━━━━━━━━━━⍟
+┏━━━━━━━━━━━━━━━━━━━⍟
+┃❥︎Creator ➪𝑆𝑡𝑖𝑔𝑚𝑎 𝑆𝐴𝑆𝐴𝐾𝐼 
+┗━━━━━━━━━━━━━━━━━━━━⍟
+┏━━━━━━━━━━━━━━━━━━━⍟
+┃❥︎WhattsApp Channel ➪https://whatsapp.com/channel/0029VaoOYCpHgZWcZwhD6V21
+┗━━━━━━━━━━━━━━━━━━━━⍟
+┏━━━━━━━━━━━━━━━━━━━⍟
+┃❥︎Group Support ➪https://chat.whatsapp.com/IdB2EfQiNlKBekQrigN9m9
+
+┗━━━━━━━━━━━━━━━━━━━━⍟
+┏━━━━━━━━━━━━━━━━━━━━⍟
+┃❥︎Please Follow My Support Channel
+┃❥︎Wanna talk to me?➪https://wa.me/242067274660 
+┗━━━━━━━━━━━━━━━━━━━━━━━⍟
+ © *2025* 𝑆𝑡𝑖𝑔𝑚𝑎 𝑆𝐴𝑆𝐴𝐾𝐼 
+____________________________________
+	
+_Don't Forget To Give Star To My Repo_`
+	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Wasi_Tech.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					WASI_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await WASI_MD_QR_CODE()
+});
+module.exports = router
